b5e60d8e0828deb728361818992cef24b0a10e5d
logroid <yoheiowa+github@gmail.com>
2021-08-31 00:35:00 +0900

    日本語化

---
+++ precache-manifest.5f014af751464fd9b59fefccf81c094c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32af69d58013400cc0338b1844a55714",
+    "url": "/lastorigin-enemy/index.html"
+  },
+  {
+    "revision": "86bb3d1e688fbb43496e",
+    "url": "/lastorigin-enemy/static/css/main.8de0710b.chunk.css"
+  },
+  {
+    "revision": "dc083779113d30e786b3",
+    "url": "/lastorigin-enemy/static/js/2.1ee4172f.chunk.js"
+  },
+  {
+    "revision": "f0ad72fb0c1c1e4a268423d8869404c9",
+    "url": "/lastorigin-enemy/static/js/2.1ee4172f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "86bb3d1e688fbb43496e",
+    "url": "/lastorigin-enemy/static/js/main.1aed2acf.chunk.js"
+  },
+  {
+    "revision": "8852b56770f96d6d7598",
+    "url": "/lastorigin-enemy/static/js/runtime-main.9787d8d6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lastorigin-index/precache-manifest.1261a569a1bea96f848373756e23f997.js"
+  "/lastorigin-enemy/precache-manifest.5f014af751464fd9b59fefccf81c094c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lastorigin-index/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/lastorigin-enemy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
